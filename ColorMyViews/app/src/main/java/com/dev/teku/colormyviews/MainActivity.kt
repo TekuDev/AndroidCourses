b54b2c5a452dbd3ee2ea8aa98a4706bf75ce2a09
TekuDruida <tekueldruida.music@gmail.com>
2020-03-14 17:48:39 +0100

    Add colored on click for the boxes

--- ColorMyViews/app/src/main/java/com/dev/teku/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/dev/teku/colormyviews/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.dev.teku.colormyviews
 
+import android.content.res.Resources
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun setListeners() {
+
+        val clickableViews: List<View> = listOf(box_one_text, box_two_text, box_three_text,
+            box_four_text, box_five_text, constraint_layout)
+
+        for (item in clickableViews) {
+
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun makeColored(view: View) = when(view.id) {
+
+        // Boxes using Color classs colors for background
+        R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+        R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+        // Boxes using Android color resources for background
+        R.id.box_three_text -> view.setBackgroundColor(
+            resources.getColor(android.R.color.holo_green_light, null))
+        R.id.box_four_text -> view.setBackgroundColor(
+            resources.getColor(android.R.color.holo_green_dark, null))
+        R.id.box_five_text -> view.setBackgroundColor(
+            resources.getColor(android.R.color.holo_green_light, null))
+
+        else -> view.setBackgroundColor(Color.LTGRAY)
     }
 }
