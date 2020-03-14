2b7b1eb20e5a02b26c932517966797d473f59313
TekuDruida <tekueldruida.music@gmail.com>
2020-03-14 18:09:33 +0100

    Add custom colors by buttons

--- ColorMyViews/app/src/main/java/com/dev/teku/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/dev/teku/colormyviews/MainActivity.kt
@@ -23,7 +23,8 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
 
         val clickableViews: List<View> = listOf(box_one_text, box_two_text, box_three_text,
-            box_four_text, box_five_text, constraint_layout)
+            box_four_text, box_five_text, constraint_layout, red_button, yellow_button,
+            green_button)
 
         for (item in clickableViews) {
 
@@ -34,7 +35,7 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.M)
     private fun makeColored(view: View) = when(view.id) {
 
-        // Boxes using Color classs colors for background
+        // Boxes using Color class colors for background
         R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
         R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
 
@@ -45,6 +46,14 @@ class MainActivity : AppCompatActivity() {
             resources.getColor(android.R.color.holo_green_dark, null))
         R.id.box_five_text -> view.setBackgroundColor(
             resources.getColor(android.R.color.holo_green_light, null))
+
+        // Boxes using custom colors for background
+        R.id.red_button -> box_three_text.setBackgroundColor(
+            resources.getColor(R.color.my_red, null))
+        R.id.yellow_button -> box_four_text.setBackgroundColor(
+            resources.getColor(R.color.my_yellow, null))
+        R.id.green_button -> box_five_text.setBackgroundColor(
+            resources.getColor(R.color.my_green, null))
 
         else -> view.setBackgroundColor(Color.LTGRAY)
     }
